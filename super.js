fc3fd921fdd8dc3d8cdb4423b5a5fac6d5b54f58
priest-2105 <fawzybailey782@gmail.com>
2024-09-06 02:11:29 +0100

    final commit ✨�

--- super.js
+++ super.js
@@ -59,7 +59,7 @@ canvas.addEventListener('mouseup', () =>{
  function drawLine(x1, y1, x2, y2){
 
    ctx.beginPath();
-   ctx.movTo(x1, y1);
+   ctx.moveTo(x1, y1);
    ctx.lineTo(x2, y2);
    ctx.strokeStyle = color;
    ctx.lineWidth = size * 2;
